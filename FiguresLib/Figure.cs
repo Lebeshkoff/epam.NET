0e2b76dd7b428d4b5956b45f3bad52201ecb2178
Лебешков Дмитрий <dmitryleb2001@gmail.com>
2020-10-25 20:04:10 +0300

    Добавьте файлы проекта.

---
+++ FiguresLib/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLib
+{
+    /// <summary>
+    /// A class that implements shapes on a plane
+    /// </summary>
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Shape perimeter
+        /// </summary>
+        /// <returns>Shape perimeter</returns>
+        public abstract double CalcP();
+        /// <summary>
+        /// Figure area
+        /// </summary>
+        /// <returns>Figure area</returns>
+        public abstract double CalcS();
+    }
+}
